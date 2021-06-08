94f38d3735d92d42a6fa04dc99e9d95bd7ab53c4
waiwai888 <yuisato9688@gmail.com>
2021-06-08 04:26:31 +0000

    [finish]3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'blogs/new'
+  get 'top' => 'homes#top'
+  post 'blogs' => 'blogs#create'
+  # For details on blogsthe DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
